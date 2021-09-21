c7a8a5d7ef4d7cdd312263f3ba25c9e99d2752b1
Kamal E J <44019738+Luci437@users.noreply.github.com>
2021-09-21 09:02:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const path = require("path")
 
 app.use(express.static(path.join(__dirname, 'build')))
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(port, ()=>console.log("Server up and running at "+port))
